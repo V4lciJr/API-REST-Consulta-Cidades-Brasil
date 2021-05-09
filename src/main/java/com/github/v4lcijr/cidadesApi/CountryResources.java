460a46cb66d1e311b49f178e0f21a14356fd9164
Valci Júnior <valcijunior19@hotmail.com>
2021-05-08 23:47:44 -0300

    alterado o retorno do metodo findAll de List para Page

--- src/main/java/com/github/v4lcijr/cidadesApi/CountryResources.java
+++ src/main/java/com/github/v4lcijr/cidadesApi/CountryResources.java
@@ -3,11 +3,12 @@ package com.github.v4lcijr.cidadesApi;
 import com.github.v4lcijr.cidadesApi.countries.Country;
 import com.github.v4lcijr.cidadesApi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -17,8 +18,9 @@ public class CountryResources {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries(){
+    public Page<Country> countries(Pageable page){
 
-        return repository.findAll();
+        return repository.findAll(page);
     }
+
 }
