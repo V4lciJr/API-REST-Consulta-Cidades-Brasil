085ed0daf895b0a90105cc11ec96b5d74b8f1eae
Valci Júnior <valcijunior19@hotmail.com>
2021-05-09 00:10:24 -0300

    criado o metodo CountryById e a classe de Excecao CountryNotFoundExceptions

--- src/main/java/com/github/v4lcijr/cidadesApi/CountryResources.java
+++ src/main/java/com/github/v4lcijr/cidadesApi/CountryResources.java
@@ -1,11 +1,13 @@
 package com.github.v4lcijr.cidadesApi;
 
 import com.github.v4lcijr.cidadesApi.countries.Country;
+import com.github.v4lcijr.cidadesApi.exceptions.CountryNotFoundExceptions;
 import com.github.v4lcijr.cidadesApi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,12 @@ public class CountryResources {
         return repository.findAll(page);
     }
 
+    @GetMapping("/{id}")
+    public Country countryById(@PathVariable Long id) throws CountryNotFoundExceptions {
+        return verifyByExists(id);
+    }
+
+    private Country verifyByExists(Long id) throws CountryNotFoundExceptions{
+        return repository.findById(id).orElseThrow(() -> new CountryNotFoundExceptions(id));
+    }
 }
---
+++ src/main/java/com/github/v4lcijr/cidadesApi/exceptions/CountryNotFoundExceptions.java
@@ -0,0 +1,12 @@
+package com.github.v4lcijr.cidadesApi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CountryNotFoundExceptions extends Exception{
+
+    public CountryNotFoundExceptions(Long id) {
+        super("Country not found with id " + id);
+    }
+}
